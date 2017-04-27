46e21795948f1b2067d113f148022d3a95d9aaad
Nils Solanki <nils.solanki@dreipol.ch>
2017-04-27 11:08:09 +0200

    [entries] Reverse order of [value, key], add range for numbers
    Throw an error if second argument is not a number

--- index.next.js
+++ index.next.js
@@ -1,9 +1,17 @@
 /**
  * Try to make loopable any kind of javascript primitive
- * @param   {array|number|string|object|Map|Set} collection - hopefully something that we can loop
+ * @param   {any} collection - hopefully something that we can loop
+ * @param   {number} to - the end of a number range
  * @returns {Array} it will return always an array
  */
-export default function looppa(collection) {
+export default function looppa(collection, to) {
+    // Special case: handle numbers
+    if (isNumber(collection) && isNumber(to)) {
+        return Array.from({ length: (to - collection + 1) }, (v, key) => [collection + key, collection + key]);
+    } else if (to) {
+        throw new TypeError('Both arguments need to be a number when creating a number range.');
+    }
+
     // handle falsy values
     if (!collection) {
         return [];
@@ -12,18 +20,23 @@ export default function looppa(collection) {
     // handle objects with an 'entries' function
     // such as: Arrays, Maps, Sets, NodeLists...
     if (typeof collection.entries === 'function') {
-        return [...collection.entries()];
-    }
-
-    // handle numbers
-    if (typeof collection === 'number') {
-        return Array.from({ length: collection }, (v, key) => [key, key]);
+        return [...collection.entries()].map(entry => entry.reverse());
     }
 
     // Default for all other object types, strings, booleans and symbols
     return Object
         .keys(collection)
         .map((key) => {
-            return [key, collection[key]];
+            return [collection[key], key];
         });
+}
+
+/**
+ * Helper function to determine if number is a Number
+ *
+ * @param  {number}  number - the value to check
+ * @return {Boolean} is it a number?
+ */
+function isNumber(number) {
+    return typeof number === 'number' && !isNaN(number);
 }
