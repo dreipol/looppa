f7142153a09d83e0a667c7a174780962b4b99072
Nils Solanki <nils.solanki@dreipol.ch>
2017-04-27 11:10:11 +0200

    [entries] Update tests to reflect [value, key] results, add error test for "to"

--- test.js
+++ test.js
@@ -15,35 +15,46 @@ describe('looppa the looper', () => {
         const iter = looppa([1, 2, 3]);
         assert.ok(iter.map);
         assert.equal(iter.length, 3);
-        assert.deepEqual(iter, [[0, 1], [1, 2], [2, 3]]);
+        assert.deepEqual(iter, [[1, 0], [2, 1], [3, 2]]);
     });
 
     it('convert numbers into arrays', () => {
-        const iter = looppa(3);
+        const iter = looppa(0, 3);
         assert.ok(iter.map);
-        assert.equal(iter.length, 3);
-        assert.deepEqual(iter, [[0, 0], [1, 1], [2, 2]]);
+        assert.equal(iter.length, 4);
+        assert.deepEqual(iter, [[0, 0], [1, 1], [2, 2], [3, 3]]);
+    });
+
+    it('convert a range of numbers into arrays', () => {
+        const iter = looppa(0, 3);
+        assert.ok(iter.map);
+        assert.equal(iter.length, 4);
+        assert.deepEqual(iter, [[0, 0], [1, 1], [2, 2], [3, 3]]);
+    });
+
+    it('throw TypeError if second argument is not a number', () => {
+        assert.throws(looppa.bind(null, 0, 'a'), TypeError);
     });
 
     it('convert strings into arrays', () => {
         const iter = looppa('ciao');
         assert.ok(iter.map);
         assert.equal(iter.length, 4);
-        assert.deepEqual(iter, [[0, 'c'], [1, 'i'], [2, 'a'], [3, 'o']]);
+        assert.deepEqual(iter, [['c', 0], ['i', 1], ['a', 2], ['o', 3]]);
     });
 
     it('convert objects into arrays', () => {
         const iter = looppa({ foo: 'bar', baz: 'buz' });
         assert.ok(iter.map);
         assert.equal(iter.length, 2);
-        assert.deepEqual(iter, [['foo', 'bar'], ['baz', 'buz']]);
+        assert.deepEqual(iter, [['bar', 'foo'], ['buz', 'baz']]);
     });
 
     it('convert Maps into arrays', () => {
         const iter = looppa(new Map().set({}, 'foo'));
         assert.ok(iter.map);
         assert.equal(iter.length, 1);
-        assert.deepEqual(iter, [[{}, 'foo']]);
+        assert.deepEqual(iter, [['foo', {}]]);
     });
 
     it('convert Sets into arrays', () => {
