afcf3d24876ca97c85981d822ef11fbc0aa5aac6
fabs <faebeee@gmail.com>
2019-05-23 09:39:03 +0200

    Improve types

--- index.next.d.ts
+++ index.next.d.ts
@@ -1,15 +1,26 @@
 declare module 'looppa' {
-    interface CallbackFn<R> {
-        (value: any, key: any, index: number): R;
+    
+    interface RangeFn<R> {
+        (index: number): R;
     }
     
-    interface FactoryFn<R> {
-        (callback: CallbackFn<R>): R[];
+    interface LetterFn<R> {
+        (letter: string, index: number): R;
     }
     
-    type Collection = number | string | object | Map<any, any> | Set<any> | [] ;
+    interface ObjectFn<R> {
+        (value: any, key: string, index: number): R;
+    }
+    interface ArrayFn<R, V> {
+        (value: V, index: number): R;
+    }
     
-    function looppa<R>(collection: Collection, to?: number): FactoryFn<R>;
+    function looppa<R>(string: string): (callback: LetterFn<R>) => R[];
+    function looppa<R>(start: number, end: number): (callback: RangeFn<R>) => R[];
+    function looppa<R, V>(array: V[]): (callback: ArrayFn<R, V>) => R[];
+    function looppa<R>(object: object): (callback: ObjectFn<R>) => R[];
+    function looppa<R>(map: Map<any, any>): (callback: ObjectFn<R>) => R[];
+    function looppa<R, V>(set: Set<any>): (callback: ArrayFn<R, V>) => R[];
     
     export = looppa;
 }
